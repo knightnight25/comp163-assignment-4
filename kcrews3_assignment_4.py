6f85ed7420ca06244810ea314a08b627f8979632
Kami Crews <150502618+knightnight25@users.noreply.github.com>
2025-09-26 23:07:26 -0400

    Pass Test Case 2: Course planning with if/elif/else and comparison operators

--- kcrews3_assignment_4.py
+++ kcrews3_assignment_4.py
@@ -10,3 +10,41 @@ print(f"GPA: {current_gpa}")
 print(f"Study hours: {study_hours}")
 print(f"Social points: {social_points}")
 print(f"Stress leves: {stress_level}")
+
+print("Choose your course load:")
+print("L) Light (12 credits)")
+print("S) Standard (15 credits)")
+print("H) Heavy (18 credits)")
+
+choice = input("Your choice: ")
+
+if choice == 'L':
+    if current_gpa >= 2.5 and current_gpa < 3.0:
+        study_hours = study_hours - 2
+        stress_level = stress_level - 25
+    else:
+        study_hours = study_hours + 1
+        stress_level = stress_level - 10
+
+elif choice == 'S':
+    if current_gpa >= 3.0 and current_gpa < 3.5:
+        study_hours  = study_hours + 2
+        stress_level = stress_level - 10
+    else:
+        study_hours = study_hours + 5
+        stress_level = stress_level - 3
+
+elif choice == 'H':
+    if current_gpa >= 3.5:
+        study_hours = study_hours + 5
+        stress_level = stress_level - 5
+    else:
+        study_hours = study_hours + 10
+        stress_level = stress_level + 5
+
+else: 
+    print("Your choice is invalid")
+
+print("New stats after course load decision")
+print(f"New weekly study hours: {study_hours}")
+print(f"New current stress levels: {stress_level}")
