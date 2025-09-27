cdcceea6664d6dd4181e09aeec740840ee377cff
Kami Crews <150502618+knightnight25@users.noreply.github.com>
2025-09-26 23:33:55 -0400

    Pass Test Case 3: Study strategy with membership and logical operators

--- kcrews3_assignment_4.py
+++ kcrews3_assignment_4.py
@@ -48,3 +48,30 @@ else:
 print("New stats after course load decision")
 print(f"New weekly study hours: {study_hours}")
 print(f"New current stress levels: {stress_level}")
+
+study_options = ["Programming", "Math", "English", "History"]
+print(f"Your study options are: {study_options} pick one: ")
+study_choice = input()
+
+if study_choice in study_options:
+    if study_choice == "Programming":
+        if current_gpa >= 3.5 or study_hours < 20:
+            current_gpa = current_gpa + 0.15
+            social_points = social_points - 5
+        elif study_choice == "Math":
+            current_gpa = current_gpa + 0.05
+            social_points = social_points - 2
+    elif study_choice == "English":
+        if not (current_gpa < 2.0):
+            social_points = social_points + 10
+            current_gpa = current_gpa + 0.02
+        elif study_choice == "History":
+            social_points = social_points - 2
+            current_gpa = current_gpa - 0.02
+elif study_choice not in study_options:
+    social_points = social_points - 3
+    current_gpa = current_gpa - 0.03
+
+print("New stats after study decision")
+print(f"New Current GPA: {current_gpa}")
+print(f"New Social Points: {social_points}")
