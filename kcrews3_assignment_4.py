fe358774c22694f11426e530c566ce9bcf457cc1
Kami Crews <150502618+knightnight25@users.noreply.github.com>
2025-09-26 22:10:32 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ kcrews3_assignment_4.py
@@ -0,0 +1,12 @@
+student_name = 'Kami Crews'
+current_gpa = 4.0 
+study_hours = 5
+social_points = 30
+stress_levels = 45
+
+print("Welcome these are the starting stats:")
+print(f"Name: {student_name}")
+print(f"GPA: {current_gpa}")
+print(f"Study hours: {study_hours}")
+print(f"Social points: {social_points}")
+print(f"Stress leves: {stress_levels}")
